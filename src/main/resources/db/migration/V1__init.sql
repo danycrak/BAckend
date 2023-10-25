837b864ccaa8e776203b534fdc6ec40d96b7f35e
danycrak <115761695+danycrak@users.noreply.github.com>
2023-10-24 20:47:42 -0500

    CRUD DE LA PRIMERA TABLA

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -5,11 +5,14 @@ CREATE TABLE IF NOT EXISTS  Cliente (
     Fecha DATE NOT NULL
     );
 
-CREATE TABLE IF NOT EXISTS Factura(
+CREATE TABLE IF NOT EXISTS Facturas(
     ID SERIAL PRIMARY KEY,
     Cliente_ID INT NOT NULL,
     Cantidad VARCHAR (50) NOT NULL,
     Ciudad VARCHAR (50) NOT NULL,
+    email VARCHAR (50) NOT NULL,
+    telefono INT NOT NULL,
+     precio INT NOT NULL,
     FOREIGN KEY (Cliente_ID) REFERENCES Cliente(ID)
 );
 
